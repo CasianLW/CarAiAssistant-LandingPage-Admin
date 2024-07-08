c8a50e9b167bb195efb6b321b0b025f30705c9dc
CasianLW <ciorbacasian@gmail.com>
2024-07-09 01:10:03 +0200

    general page padding

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="p-2">
         <Main />
         <NextScript />
       </body>
