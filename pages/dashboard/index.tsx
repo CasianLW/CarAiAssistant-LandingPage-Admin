4f03f9dc9c489ba603286c74579f10785a523ec1
CasianLW <ciorbacasian@gmail.com>
2024-07-08 17:34:51 +0200

    auth page & logic working to set token to localstorage

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,10 +1,15 @@
 "use client";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import useAdminAuth from "@/hooks/use-auth.hook";
+import { useApiHelper } from "@/helpers/api-helper.hook";
+import { Users } from "@/components/Users.component";
+import { Settings } from "@/components/Settings.component";
 
 export default function AdminDashboard() {
   const { isAuthorized, isChecking } = useAdminAuth();
+  //   const { sendRequest } = useApiHelper();
+  const [activeTab, setActiveTab] = useState("users"); // 'users' or 'settings'
   const router = useRouter();
 
   useEffect(() => {
@@ -14,16 +19,56 @@ export default function AdminDashboard() {
   }, [isAuthorized, isChecking, router]);
 
   const handleLogout = () => {
-    localStorage.removeItem("token"); // Remove the token from local storage
-    router.push("/auth"); // Redirect to the login page
+    localStorage.removeItem("token");
+    router.push("/auth");
+  };
+
+  const renderContent = () => {
+    switch (activeTab) {
+      case "users":
+        return <Users />;
+      case "settings":
+        return <Settings />;
+      default:
+        return <p>No content available.</p>;
+    }
   };
 
   return isAuthorized ? (
-    <main>
-      <h1>Admin Dashboard</h1>
-      <div>Welcome, Admin!</div>
-      <button onClick={handleLogout}>Logout</button> {/* Logout Button */}
-    </main>
+    <div className="min-h-screen bg-gray-100">
+      <header className="bg-white shadow">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
+          <h1 className="text-2xl font-bold text-gray-900">Admin Dashboard</h1>
+          <button
+            onClick={handleLogout}
+            className="mt-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Logout
+          </button>
+        </div>
+      </header>
+      <main>
+        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+          <div className="flex gap-4 mb-5 justify-around">
+            <button
+              onClick={() => setActiveTab("users")}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50"
+              disabled={activeTab === "users"}
+            >
+              Manage Users
+            </button>
+            <button
+              onClick={() => setActiveTab("settings")}
+              disabled={activeTab === "settings"}
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50"
+            >
+              Manage Settings
+            </button>
+          </div>
+          <div className="mx-2">{renderContent()}</div>
+        </div>
+      </main>
+    </div>
   ) : (
     <p>Loading...</p>
   );
